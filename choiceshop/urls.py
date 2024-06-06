e91639fcc96bd79494329ba1bbc44cc578e8d652
Vikask009 <clothifyind@gmail.com>
2024-06-06 16:52:08 +0530

    store and cart functionality

--- choiceshop/urls.py
+++ choiceshop/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homeapp.urls')),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 
 
 
